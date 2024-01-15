e950056f26417ae3463c68f2a2c619b73ee82a59
Sam Puttman <daputter2@gmail.com>
2024-01-15 18:33:43 -0500

    Write model and feature tests for update_status methods.

--- spec/features/coupons/show_spec.rb
+++ spec/features/coupons/show_spec.rb
@@ -6,13 +6,27 @@ RSpec.describe Coupon do
     end
 
     it "shows correct coupon information" do
-
       visit merchant_coupon_path(@merchant, @coupon1)
   
       expect(page).to have_content(@coupon1.name)
       expect(page).to have_content(@coupon1.code)
       expect(page).to have_content("Discount: 10")
-      expect(page).to have_content("Status: Active")
+      expect(page).to have_content("Status: active")
+    end
+
+    it "allows merchants to toggle coupon status" do
+      visit merchant_coupon_path(@merchant, @coupon1)
+
+      expect(page).to have_content(@coupon1.name)
+      expect(page).to have_content(@coupon1.code)
+      expect(page).to have_content("Discount: 10")
+      expect(page).to have_content("Status: active")
+
+      expect(page).to have_content("Deactivate Coupon")
+      click_button "Deactivate Coupon"
+      
+      visit current_path
+      expect(page).to have_content("Status: inactive")
     end
   end
 end
