ce03bdddc8aa4c115c539cbf99292b4a327f1bfd
Sam Puttman <daputter2@gmail.com>
2024-01-15 17:16:47 -0500

    Implement coupon new and create methods in controller.

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -3,4 +3,28 @@ class CouponsController < ApplicationController
     @merchant = Merchant.find(params[:merchant_id])
     @coupons = @merchant.coupons
   end
+
+  def new
+    @merchant = Merchant.find(params[:merchant_id])
+    @coupon = Coupon.new
+  end
+
+  def create
+    @merchant = Merchant.find(params[:merchant_id])
+    @coupon = @merchant.coupons.new(coupon_params)
+  
+    if @coupon.save
+      redirect_to merchant_coupons_path(@merchant), notice: "Coupon created successfully."
+    else
+      flash[:alert] = @coupon.errors.full_messages.to_sentence
+      redirect_to new_merchant_coupon_path(@merchant)
+    end
+  end
+
+  private
+
+  def coupon_params
+    params.require(:coupon).permit(:name, :code, :discount_value, :discount_type)
+  end
+
 end
