826733a2e1b83da2e462260cee14a216de876abe
Sam Puttman <daputter2@gmail.com>
2024-01-15 14:45:05 -0500

    Made discount type an enum - may revert later.

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -6,4 +6,7 @@ class Coupon < ApplicationRecord
   validates :code, presence: true, uniqueness: true
   validates :discount_value, presence: true
   validates :discount_type, presence: true
+
+  enum discount_type: { percent_off: 0, dollar_off: 1 }
+
 end
