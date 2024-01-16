b346f7b70d7c561b71c27981398d4c5b34c83390
Sam Puttman <daputter2@gmail.com>
2024-01-16 18:35:48 -0500

    Add method to calculate grand total. Lots of little private methods to do the math.

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -14,4 +14,33 @@ class Invoice < ApplicationRecord
   def total_revenue
     invoice_items.sum("unit_price * quantity")
   end
+
+  def grand_total
+    if coupon
+      total_revenue - applied_discount
+    else
+      total_revenue
+    end
+  end
+
+  private
+
+  def applied_discount
+    return 0 unless coupon
+
+    if coupon.discount_type == 'percent_off'
+      calculate_percentage_discount
+    else coupon.discount_type == 'dollar_off'
+      calculate_fixed_discount
+    end
+  end
+
+  def calculate_percentage_discount
+    total_revenue * (coupon.discount_value.to_f / 100)
+  end
+
+  def calculate_fixed_discount
+    [coupon.discount_value, total_revenue].min
+  end
 end
+
